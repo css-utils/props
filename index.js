070ecc2562d34c3201c64e93a3a9a7a41f3419dc
camshaft <bytheway.cameron@gmail.com>
2016-01-31 13:52:40 -0700

    update list

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var props = require('./props.json');
-for (var i = 0; i < props.length; i++) {
-  props[props[i]] = true;
+var propList = require('./props.json');
+var props = {};
+for (var i = 0, name; i < propList.length; i++) {
+  name = propList[i];
+  propList[name] = props[name] = true;
 };
 
 exports = module.exports = function isValidProp(prop) {
   return (typeof prop === 'string') && (props[prop] || /^--.*/.test(prop) || /^-(webkit|ms|moz|o|khtml|epub|internal)/.test(prop));
 };
 exports['default'] = exports;
-exports.props = props;
+exports.props = propList;
